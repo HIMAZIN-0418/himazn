965b3a6561c010e5a816e8c11afa63631608f37e
暇人 <kimura20110418@gmail.com>
2026-04-06 12:43:56 +0900

    Update script.js

--- himazn-site/script.js
+++ himazn-site/script.js
@@ -1,1 +1,13 @@
+function setLang(lang) {
+  const elements = document.querySelectorAll("[data-ja]");
+  elements.forEach(e1 => {
+    el.textContent = el.getAttrobute("data-"+lamg);
+  });
 
+const slider = document.querySelector("slider");
+  if (lane === "ja") slider.style.left = "0%";
+  if (lane === "en") slider.style.left = "33.3%";
+  if (lane === "zh") slider.style.left = "66.6%";
+}
+
+setLang("ja");
